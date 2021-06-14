5162f54399c51218761071638a5b5204dea82344
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-06-14 22:15:18 +0530

    setting up the ejs as template engine

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const port = 8000;
-
+const path = require('path');
+const port = 7000;
 const app = express();
+
+app.set('view-engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 app.listen(port, function(err){
     if(err){ 
         console.log('Error in running the server');
