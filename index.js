5990e3f1b967dbe52547cc262f5d52e88fb88fb8
Piyanshu Mangla <piyanshumangla456@gmail.com>
2021-06-14 22:40:40 +0530

    handles the first request and passing the  form in response

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const path = require('path');
 const port = 7000;
 const app = express();
 
-app.set('view-engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.get('/', function(req, res){
+    return res.render('home', {
+        title : 'Contact List'
+    });
+    // return res.end('Gotcha!');
+});
 
 app.listen(port, function(err){
     if(err){ 
